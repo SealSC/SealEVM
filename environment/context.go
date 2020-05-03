0bb44e8b625ee59b049b0b082edf9b5fc9c5f9d1
AKACoder <akacoder@outlook.com>
2020-05-03 17:06:32 +0800

    rename package context to environment

--- environment/context.go
+++ environment/context.go
@@ -14,7 +14,7 @@
  *  limitations under the License.
  */
 
-package context
+package environment
 
 type Context struct {
 	Block
--- environment/message.go
+++ environment/message.go
@@ -14,7 +14,7 @@
  *  limitations under the License.
  */
 
-package context
+package environment
 
 type Message struct {
 	Caller  []byte
